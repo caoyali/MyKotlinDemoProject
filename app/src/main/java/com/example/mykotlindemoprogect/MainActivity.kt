9a96e628d339405ef39f1711c6b56b467f8bdff1
caoyali <caoyali@gmtech.ai>
2019-04-26 10:13:53 +0800

    将代码调通

--- app/src/main/java/com/example/mykotlindemoprogect/MainActivity.kt
+++ app/src/main/java/com/example/mykotlindemoprogect/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.mykotlindemoprogect
 
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , BlankFragment.OnFragmentInteractionListener{
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp() =
+        Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
 }
