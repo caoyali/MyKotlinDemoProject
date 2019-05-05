306f11f681b00da192e7fc1a7fe3fd4e16f79ad5
caoyali <caoyali@gmtech.ai>
2019-05-05 11:50:29 +0800

    feature: 提交第一个界面

--- app/src/main/java/com/example/mykotlindemoprogect/MainActivity.kt
+++ app/src/main/java/com/example/mykotlindemoprogect/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.example.mykotlindemoprogect
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
 
-class MainActivity : AppCompatActivity() , BlankFragment.OnFragmentInteractionListener{
-    override fun onFragmentInteraction(uri: Uri) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     override fun onSupportNavigateUp() =
-        Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
+        findNavController(this, R.id.my_nav_host_fragment).navigateUp()
 }
